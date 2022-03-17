371b9301792218c92aa0a58df19aeba44ce22c3b
yash bhangale <68491303+yashbhangale9@users.noreply.github.com>
2022-03-17 23:42:00 +0530

    Update volumeofcylinder.c

--- volumeofcylinder.c
+++ volumeofcylinder.c
@@ -3,9 +3,9 @@ int main()
 {
     int radius = 3;
     float pi = 3.14;
-    printf("The area of this circle is %f", pi * radius);
+    printf("The area of this circle is %f\n", pi * radius);
     int height = 3;
-    printf("Volume of this cylinder is %f", pi * radius*radius*height);
+    printf("Volume of this cylinder is %f\n", pi * radius*radius*height);
                                                     
     return 0;
 }
